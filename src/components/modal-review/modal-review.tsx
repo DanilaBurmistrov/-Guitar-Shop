5fb1de286ebd94f3d5dbba13895525941fa35a89
Danila Burmistrov <burmistrov.danila2016@yandex.ru>
2022-05-27 15:03:57 +0300

    правки после тестирования

--- src/components/modal-review/modal-review.tsx
+++ src/components/modal-review/modal-review.tsx
@@ -140,35 +140,35 @@ export default function ModalReview({setIsSuccessReviewModalOpened, setIsFormMod
                 <p className="form-review__warning" style={{opacity: `${invalidName ? 1 : 0}`}}>Заполните поле</p>
               </div>
               <div><span className="form-review__label form-review__label--required">Ваша Оценка</span>
-                <div className="rate rate--reverse" tabIndex={3}>
-                  <input className="visually-hidden" id="star-5" name="rate" type="radio" value="5" />
+                <div className="rate rate--reverse">
+                  <input className="visually-hidden" id="star-5" name="rate" type="radio" value="5" tabIndex={3}/>
                   <label className="rate__label" htmlFor="star-5" title="Отлично"></label>
-                  <input className="visually-hidden" id="star-4" name="rate" type="radio" value="4" />
+                  <input className="visually-hidden" id="star-4" name="rate" type="radio" value="4" tabIndex={4}/>
                   <label className="rate__label" htmlFor="star-4" title="Хорошо"></label>
-                  <input className="visually-hidden" id="star-3" name="rate" type="radio" value="3" />
+                  <input className="visually-hidden" id="star-3" name="rate" type="radio" value="3" tabIndex={5}/>
                   <label className="rate__label" htmlFor="star-3" title="Нормально"></label>
-                  <input className="visually-hidden" id="star-2" name="rate" type="radio" value="2" />
+                  <input className="visually-hidden" id="star-2" name="rate" type="radio" value="2" tabIndex={6}/>
                   <label className="rate__label" htmlFor="star-2" title="Плохо"></label>
-                  <input className="visually-hidden" id="star-1" name="rate" type="radio" value="1" />
+                  <input className="visually-hidden" id="star-1" name="rate" type="radio" value="1" tabIndex={7}/>
                   <label className="rate__label" htmlFor="star-1" title="Ужасно"></label>
                   <p className="rate__message" style={{opacity: `${invalidRate ? 1 : 0}`}}>Поставьте оценку</p>
                 </div>
               </div>
             </div>
             <label className="form-review__label form-review__label--required" htmlFor="adv">Достоинства</label>
-            <input className="form-review__input" id="adv" type="text" name="advantage" data-testid="advantage" autoComplete="off" tabIndex={4}/>
+            <input className="form-review__input" id="adv" type="text" name="advantage" data-testid="advantage" autoComplete="off" tabIndex={8}/>
             <p className="form-review__warning" style={{opacity: `${invalidAdvantage ? 1 : 0}`}}>Заполните поле</p>
             <label className="form-review__label form-review__label--required" htmlFor="disadv">Недостатки</label>
-            <input className="form-review__input" id="disadv" type="text" name="disadvantage" data-testid="disadvantage" autoComplete="off" tabIndex={5}/>
+            <input className="form-review__input" id="disadv" type="text" name="disadvantage" data-testid="disadvantage" autoComplete="off" tabIndex={9}/>
             <p className="form-review__warning" style={{opacity: `${invalidDisadvantage ? 1 : 0}`}}>Заполните поле</p>
             <label className="form-review__label form-review__label--required" htmlFor="comment">Комментарий</label>
-            <textarea className="form-review__input form-review__input--textarea" id="comment" name="comment" rows={10} data-testid="comment" autoComplete="off" tabIndex={6}></textarea>
+            <textarea className="form-review__input form-review__input--textarea" id="comment" name="comment" rows={10} data-testid="comment" autoComplete="off" tabIndex={10}></textarea>
             <p className="form-review__warning" style={{opacity: `${invalidComment ? 1 : 0}`}}>Заполните поле</p>
-            <button className="button button--medium-20 form-review__button" type="submit" disabled={isSaving} tabIndex={7}>{isSaving ? <LoadingScreen /> : 'Отправить отзыв'}</button>
+            <button className="button button--medium-20 form-review__button" type="submit" disabled={isSaving} tabIndex={11}>{isSaving ? <LoadingScreen /> : 'Отправить отзыв'}</button>
           </form>
-          <button className="modal__close-btn button-cross" data-testid="close" type="button" aria-label="Закрыть" onClick={() => setIsFormModalOpened(false)}><span className="button-cross__icon" ref={lastAutofocusRef} tabIndex={8}></span><span className="modal__close-btn-interactive-area"></span>
+          <button className="modal__close-btn button-cross" data-testid="close" type="button" aria-label="Закрыть" onClick={() => setIsFormModalOpened(false)} ref={lastAutofocusRef} tabIndex={12}><span className="button-cross__icon"></span><span className="modal__close-btn-interactive-area"></span>
           </button>
-          <span tabIndex={9} onFocus={(evt) => {firstAutofocusRef.current?.focus();}}/>
+          <span tabIndex={13} onFocus={() => {firstAutofocusRef.current?.focus();}}/>
         </div>
       </div>
     </div>
